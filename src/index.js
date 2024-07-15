f37bc8f4d3ffe63af2aa0a893ccbd4561bb0eb84
Mohammed Saif <saif@grenerobotics.com>
2024-07-15 14:46:30 +0530

    Connect to mongoose atlas way II

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+dotenv.config({
+  path: './env',
+});
+
+connectDB();
+
+/*
+import express from 'express';
+require('dotenv').config({ path: './env' });
 import mongoose from 'mongoose';
 import { DB_NAME } from './constants';
-
-import express from 'express';
 const app = express()(async () => {
   try {
     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
@@ -18,3 +27,4 @@ const app = express()(async () => {
     throw err;
   }
 })();
+*/
