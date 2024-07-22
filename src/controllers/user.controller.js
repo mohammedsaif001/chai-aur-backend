269f009d3216351d4faad15e9f9418dfae436e92
Mohammed Saif <saif@grenerobotics.com>
2024-07-22 15:37:45 +0530

    changeCurrentPassword, getCurrentUser, updateAccount controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -242,4 +242,58 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentPassword = asyncHandler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+  const user = await User.findById(req?.user?._id);
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, 'Invalid Old Password');
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, 'Password updated successfully'));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res.status.json(200, req?.user, 'Current User Fetched Successfully');
+});
+
+const updateAccount = asyncHandler(async (req, res) => {
+  const { fullName, email } = req?.body;
+
+  if (!fullName || !email) {
+    throw new ApiError(400, 'All fields are required');
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req?.user?._id,
+    {
+      $set: {
+        fullName,
+        email,
+      },
+    },
+    {
+      new: true,
+    }
+  ).select('-password');
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, 'Account details updated successfully'));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccount,
+};
