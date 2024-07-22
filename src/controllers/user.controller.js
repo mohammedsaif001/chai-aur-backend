7cc2caa9dbc0a38a7d18105fec71688705ed48ac
Mohammed Saif <saif@grenerobotics.com>
2024-07-22 15:47:46 +0530

    updateUserCoverImage, updateUserAvatar Controllers

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -288,6 +288,60 @@ const updateAccount = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, user, 'Account details updated successfully'));
 });
 
+const updateUserAvatar = asyncHandler(async (req, res) => {
+  const avatarLocalPath = req?.file?.path;
+  if (!avatarLocalPath) {
+    throw new ApiError(400, 'Avatar File is missing');
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+
+  if (!avatar.url) {
+    throw new ApiError(400, 'Error while uploading avatar');
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req?.user?._id,
+    {
+      $set: {
+        avatar: avatar.url,
+      },
+    },
+    { new: true }
+  ).select('-password');
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, 'Avatar Updated Successfully'));
+});
+
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req?.file?.path;
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, 'Cover Image File is missing');
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new ApiError(400, 'Error while uploading cover Image');
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req?.user?._id,
+    {
+      $set: {
+        coverImage: coverImage.url,
+      },
+    },
+    { new: true }
+  ).select('-password');
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, user, 'Cover Image Updated Successfully'));
+});
+
 export {
   registerUser,
   loginUser,
@@ -296,4 +350,6 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccount,
+  updateUserAvatar,
+  updateUserCoverImage,
 };
