0b05a1b3116bda21db65ba11b146d749175be2a3
Mohammed Saif <saif@grenerobotics.com>
2024-07-22 17:21:05 +0530

    Adding Routes for the controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,16 @@
 import { Router } from 'express';
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
+  refreshAccessToken,
   registerUser,
+  updateAccount,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js';
 import { verifyJWT } from '../middlewares/auth.middleware.js';
@@ -23,5 +31,17 @@ userRouter.route('/login').post(loginUser);
 // Secured routes
 userRouter.route('/logout').post(verifyJWT, logoutUser);
 userRouter.route('/refresh-token').post(verifyJWT, refreshAccessToken);
+userRouter.route('/change-password').post(verifyJWT, changeCurrentPassword);
+userRouter.route('/current-user').get(verifyJWT, getCurrentUser);
+userRouter.route('/update-account').patch(verifyJWT, updateAccount);
+// file uploads
+userRouter
+  .route('/avatar')
+  .patch(verifyJWT, upload.single('avatar'), updateUserAvatar);
+userRouter
+  .route('/coverImage')
+  .patch(verifyJWT, upload.single('coverImage'), updateUserCoverImage);
+userRouter.route('/channel/:username').get(verifyJWT, getUserChannelProfile);
+userRouter.route('/history').get(verifyJWT, getWatchHistory);
 
 export default userRouter;
